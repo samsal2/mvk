4280c312b63fae817b0b9c79c93e92793d4041ac
samsal2 <samsal9681@gmail.com>
2021-06-12 00:34:37 -0500

    utility: fix concepts

--- mvk/utility/concepts.hpp
+++ mvk/utility/concepts.hpp
@@ -11,15 +11,15 @@ namespace mvk::utility
 template <typename This, typename That>
 concept same_as = requires
 {
-    {std::is_same_v<This, That>};
-    {std::is_same_v<That, This>};
+    requires std::is_same_v<This, That>;
+    requires std::is_same_v<That, This>;
 };
 
 template <typename Other, typename This>
 concept not_this = requires
 {
-    {!std::is_same_v<Other, std::decay_t<This>>};
-    {!std::is_base_of_v<Other, std::decay_t<This>>};
+    requires !std::is_same_v<Other, std::decay_t<This>>;
+    requires !std::is_base_of_v<Other, std::decay_t<This>>;
 };
 
 template <typename Container>
@@ -37,8 +37,8 @@ concept with_size = requires(Container container)
 template <typename Container>
 concept with_data_and_size = requires
 {
-    {with_data<Container>};
-    {with_size<Container>};
+    requires with_data<Container>;
+    requires with_size<Container>;
 };
 
 namespace detail
@@ -56,14 +56,14 @@ using value_type_from_data_t =
 template <typename From, typename To>
 concept convertible_as_array_to = requires
 {
-    {std::is_convertible_v<From(*)[], To(*)[]>};
+    requires std::is_convertible_v<From(*)[], To(*)[]>;
 };
 
 template <typename Container, typename Element, typename Value = detail::value_type_from_data_t<Container>>
 concept compatible_with_element = requires
 {
-    {with_data<Container>};
-    {convertible_as_array_to<Value, Element>};
+    requires with_data<Container>;
+    requires convertible_as_array_to<Value, Element>;
 };
 
 namespace detail
@@ -73,9 +73,7 @@ namespace detail
 template <typename Iterator, typename IteratorTag>
 concept match_iterator_tag = requires
 {
-  {std::is_same_v<
-    typename std::iterator_traits<Iterator>::iterator_category,
-    IteratorTag>};
+  requires std::is_same_v<typename std::iterator_traits<Iterator>::iterator_category, IteratorTag>;
 };
 
     // clang-format on
@@ -84,7 +82,7 @@ concept match_iterator_tag = requires
 template <typename Iterator>
 concept random_access = requires
 {
-    {detail::match_iterator_tag<Iterator, std::random_access_iterator_tag>};
+    requires detail::match_iterator_tag<Iterator, std::random_access_iterator_tag>;
 };
 
 template <typename Callable, typename Returns, typename... Args>
